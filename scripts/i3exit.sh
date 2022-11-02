4148267f3ef4e7cb86f5323f19829a85c1b43883
Charles Gueunet <charles.gueunet@gmail.com>
2022-11-02 13:38:03 +0100

    New lock

--- scripts/i3exit.sh
+++ scripts/i3exit.sh
@@ -5,25 +5,13 @@
 
 case "$1" in
     lock)
-        xtrlock
+        betterlockscreen -l dim
         ;;
     logout)
         i3-msg exit
         ;;
-    suspend)
-        systemctl suspend
-        ;;
-    hibernate)
-        systemctl hibernate
-        ;;
-    reboot)
-        systemctl reboot
-        ;;
-    shutdown)
-        systemctl poweroff
-        ;;
     *)
-        echo "Usage: $0 {lock|logout|suspend|hibernate|reboot|shutdown}"
+        echo "Usage: $0 {lock|logout}"
         exit 2
 esac
 
